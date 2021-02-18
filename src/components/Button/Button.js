3ea369ebf434b218388bffc7bd9d9bf806116f1d
nikolay-litvin <nikolay.litvin@gmail.com>
2021-02-18 06:32:10 +0200

    last

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import openexchange from '../../api/openexchange';
 
 import RadioButton from '../RadioButton';
